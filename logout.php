d969574a320ac1b3e84509eb116f3b7172dd2ddf
Campus FP <user@gmail.com>
2024-01-26 12:33:15 +0100

    Primer envio

---
+++ logout.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+//guardar en base de datos
+$conexion=new PDO("mysql:host=localhost;dbname=test","root","");
+$consulta=$conexion->prepare("update usuarios set info=:info where nombre=:nombre");
+$consulta->bindParam(":info",$_SESSION['consulta']);
+$consulta->bindParam(":nombre",$_SESSION['nombre']);
+$consulta->execute();
+// Destruye todas las variables de sesión
+session_unset();
+session_destroy();
+
+// Redirige a la página de inicio
+header("Location: index.php");
+exit();
